6200a93b0cfd3d754cad1f2a773c8f7aedeb074f
ixyzdev <ixyz.dev@gmail.com>
2025-03-27 11:12:15 -0300

    feat(actions): problemas con las iamgenes en el deploy

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,10 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   basePath: isProd ? "/schemify.com" : "",
   assetPrefix: isProd ? "/schemify.com/" : "",
+  images: {
+    loader: "custom",
+    path: isProd ? "/schemify.com/" : "/",
+  },
 };
 
 export default nextConfig;
